3aa64c682987da28c3cf93eb36daa58b41be4916
ATK <ATKmain@gmail.com>
2026-01-14 17:02:25 +1100

    v0.6.0-beta.2: Fix install script overwrite issue
    By Agentive (www.agentive.au)

--- server/version.ts
+++ server/version.ts
@@ -1,3 +1,3 @@
 // This file is auto-generated during build
 // DO NOT EDIT MANUALLY - it will be overwritten by build-release.sh
-export const APP_VERSION = "0.6.0-beta.1";
+export const APP_VERSION = "0.6.0-beta.2";
